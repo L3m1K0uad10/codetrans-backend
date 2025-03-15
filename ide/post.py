820aeee25fd493a3b6db02c956e8fbfcd7fe2c77
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-03-15 21:01:17 +0530

    updated file

--- ide/post.py
+++ ide/post.py
@@ -15,7 +15,8 @@ endpoint = "http://127.0.0.1:8000/processing/translate/"
 data = {
     "file_name": "code.py",  # You can send metadata, like the file name
     "language": "python",  # If you want to send the programming language or other info
-    "file_content": file_content
+    "file_content": file_content,
+    "level": "Partial"
 }
 
 # Send POST request with the content as JSON
